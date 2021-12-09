80182277f6f9e674282eae062eb4387ccdcb8a3a
anton <antonsalekseev@gmail.com>
2021-12-09 13:40:17 +0700

    Создание релиза

--- src/test/java/TestMethods.java
+++ src/test/java/TestMethods.java
@@ -1,4 +1,5 @@
 
+import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
 import ru.kamatech.qaaf.database.JDBI;
 import ru.kamatech.qaaf.properties.Properties;
@@ -12,13 +13,15 @@ public class TestMethods extends Properties  {
     @Test
     public void testMethods(){
 
-        //JDBI jdbi = new JDBI();
+        JDBI jdbi = new JDBI();
         //DriverManager.getConnection("jdbc:h2:/data/test;AUTO_SERVER=TRUE");
         //jdbi.setDataBaseSettings("jdbc:sqlite:"+Properties.getPathFromResources("englishWords.db"),null,null);
         //jdbi.setDataBaseSettings("jdbc:h2:"+Properties.getPathFromResources("englishWordsH2.mv.db"),"admin","123456");
 
         //jdbi.setDataBaseSettings("jdbc:h2:tcp://localhost/~/englishWordsH2","admin","123456");
-        //System.out.println(jdbi.getAllRowsFromResponse(Collections.emptyList(),"select* from words", false));
+        //jdbi.setDataBaseSettings("jdbc:h2:http://127.0.1.1:9092/~/englishWordsH2", "admin", "123456");
+        //System.out.println(jdbi.getAllRowsFromResponse(Collections.emptyList(),"show tables", false));
+
 //        jdbi.createUpdate(Arrays.asList(2222, "qqqwwwww - ягода"),"insert into words (chatId, word) values (?,?)",false);
 //        jdbi.createUpdate(Arrays.asList(43334333, "erwsefdfgdf - авпвапваввввв"),"insert into words (chatId, word) values (?,?)",false);
 //        jdbi.createUpdate(Arrays.asList(34555555, "sfdsdfsdfdsfffff - орпавчмссчч"),"insert into words (chatId, word) values (?,?)",false);
